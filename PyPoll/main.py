24f53099d20caa255021fc9d89ea40bf8b44ec0f
amydst <amy.dst@gmail.com>
2024-08-20 22:00:16 -0700

    Pypoll finished, needs cleaning, create functions?

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,60 +1,71 @@
-#import os
-#import csv
+import os
+import csv
 
+#Path to collect data from the Resources folder
 
-# Path to collect data from the Resources folder
-#election_csv = os.path.join('Resources', 'election_data.csv')
+election_csv = os.path.join('Resources', 'election_data.csv')
 
 # Open the election data file
-# with open(election_csv,'r') as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=",")
+with open(election_csv,'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
 
-#     # Read the header row first
-#     csv_header = next(csv_file)
+# Read the header row first
+    csv_header = next(csv_file)
 
-     # I like to explicitly declare all my variables
-
-csv_reader = []
-csv_reader=[2405206,"Denver","Charles Casper Stockham"],[2161244,"Denver","Charles Casper Stockham"],[2795423,"Denver","Charles Casper Stockham"],[2549433,"Denver","Charles Casper Stockham"], [2429624,"Denver","Charles Casper Stockham"],[2348406,"Denver","Charles Casper Stockham"],[6459937,"Denver","Diana DeGette"],[7586282,"Denver","Diana DeGette"],[7272742,"Denver","Diana DeGette"],[5361931,"Denver","Diana DeGette"],[6072352,"Denver","Diana DeGette"],[5702090,"Denver","Diana DeGette"],[6586685,"Denver","Diana DeGette"],[7822125,"Denver","Diana DeGette"],[7323845,"Denver","Diana DeGette"]
-total_votes = 0
-candidate_results = {}
-candidate_total = 0
-candidate_name = ""
+# I like to explicitly declare all my variables
+    total_votes = 0
+    candidate_results = {}
+    candidate_total = 0
+    candidate_name = ""
 
 #Iterates over each row and calculates the different values requested
+    for row in csv_reader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name in candidate_results:
+            candidate_results[candidate_name] += 1
+        else:
+            candidate_results[candidate_name] = 1
 
-#candidate_list = [candidate_name,0]
+print("\n Election Results: \n")
+print("--------------------------\n")
+print("Total votes: " + str(total_votes) +"\n")
+print("--------------------------\n")
 
-for row in csv_reader:
-    total_votes += 1
-    candidate_name = row[2]
-    if candidate_name in candidate_results:
-        candidate_results[candidate_name] += 1
-    else:
-        candidate_results[candidate_name] = 1
-
-print("List of candidates who received votes:")
-
-print(candidate_results)
+winner_votes = 0
 for candidate_name, votes in candidate_results.items():
-    print(f"{candidate_name}: {votes} votes")
+    if votes > winner_votes:
+        winner_votes = votes
+        winner_name = candidate_name
 
-print(f"Total Votes: {total_votes}")
+    percentage = votes/total_votes
+    #format(percentage, ".2%")
+    print(f"{candidate_name}: {votes} votes ({percentage:.2%})\n")
 
-    #candidate_list.append(candidate_name, candidate_total)
+print("--------------------------\n")
+print("Winner:" + winner_name +"\n")
+print("--------------------------\n")
 
-    #if candidate_list:
-    # if str(candidate_name) == str(candidate_list[0]):
-    #     candidate_total =+ 1
+with open('ElectionResult.txt', 'w') as txt_file:
+
+    txt_file.write("\nElection Results: \n")
+    txt_file.write("--------------------------\n")
+    txt_file.write("Total votes: " + str(total_votes) +"\n")
+    txt_file.write("--------------------------\n")
+
+    winner_votes = 0
+    for candidate_name, votes in candidate_results.items():
+        if votes > winner_votes:
+            winner_votes = votes
+            winner_name = candidate_name
+
+        percentage = votes/total_votes
         
-    #     #temp_list = ([candidate_name,candidate_total])
-    #     #candidate_list.append(temp_list)
-    # else:
-    #     print("not it isn't")
-    #     candidate_list.append([candidate_name,candidate_total])
-    #     #candidate_list.append({row[2],0})
+        txt_file.write(f"{candidate_name}: {votes} votes ({percentage:.2%})\n")
+
+    txt_file.write("--------------------------\n")
+    txt_file.write("Winner: " + winner_name +"\n")
+    txt_file.write("--------------------------\n")
+
+print("Output saved to ElectionResult.txt")
     
-
-#print(candidate_results)
-#print(total_votes)
-
